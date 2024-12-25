b5e9fe03a6943535d7e7d7cbffa00e2338b14d15
Fraol <88104941+fraolme@users.noreply.github.com>
2024-12-25 15:38:44 +0300

    parser: implement Display for AST nodes for debugging purposes

--- src/ast.rs
+++ src/ast.rs
@@ -1,7 +1,8 @@
 use crate::token::Token;
 use std::fmt;
+use std::fmt::Write;
 
-pub trait Node {
+pub trait Node : fmt::Display {
     fn token_literal(&self) -> &str;
     fn node_type(&self) -> NodeType;
 }
@@ -12,6 +13,7 @@ pub enum NodeType {
     Let,
     Identifier,
     Return,
+    ExpressionStatement,
 }
 
 impl fmt::Display for NodeType {
@@ -21,28 +23,43 @@ impl fmt::Display for NodeType {
             NodeType::Let => "let",
             NodeType::Identifier => "identifier",
             NodeType::Return => "return",
+            NodeType::ExpressionStatement => "expression_statement",
         };
 
         write!(f, "{}", val)
     }
 }
+
 pub enum Statement {
     Let(LetStatement),
     Return(ReturnStatement),
+    Expression(ExpressionStatement),
 }
 
 impl Node for Statement {
     fn token_literal(&self) -> &str {
         match self {
-            Statement::Let(let_statement) => let_statement.token_literal(),
-            Statement::Return(return_statement) => return_statement.token_literal(),
+            Statement::Let(let_stmt) => let_stmt.token_literal(),
+            Statement::Return(return_stmt) => return_stmt.token_literal(),
+            Statement::Expression(expr_stmt) => expr_stmt.token_literal(),
         }
     }
 
     fn node_type(&self) -> NodeType {
         match self {
-            Statement::Let(let_statement) => let_statement.node_type(), 
-            Statement::Return(return_statement) => return_statement.node_type(),
+            Statement::Let(let_stmt) => let_stmt.node_type(), 
+            Statement::Return(return_stmt) => return_stmt.node_type(),
+            Statement::Expression(expr_stmt) => expr_stmt.node_type(),
+        }
+    }
+}
+
+impl fmt::Display for Statement {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Statement::Let(let_stmt) => write!(f, "{}", let_stmt),
+            Statement::Return(return_stmt) => write!(f, "{}", return_stmt),
+            Statement::Expression(expr_stmt) => write!(f, "{}", expr_stmt),
         }
     }
 }
@@ -65,6 +82,14 @@ impl Node for Expression {
     }
 }
 
+impl fmt::Display for Expression {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Expression::Ident(identifier) => write!(f, "{}", identifier)
+        }
+    }
+}
+
 pub struct Program {
     pub statements: Vec<Statement>,
 }
@@ -80,6 +105,17 @@ impl Node for Program {
 
     fn node_type(&self) -> NodeType {
         NodeType::Program
+    }
+}
+
+impl fmt::Display for Program {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut buffer = String::new();
+        for stmt in &self.statements {
+            write!(&mut buffer, "{}", stmt).unwrap();
+        }
+
+        write!(f, "{}", buffer)
     }
 }
 
@@ -100,6 +136,18 @@ impl Node for LetStatement {
     }
 }
 
+impl fmt::Display for LetStatement {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut buffer = String::new();
+        write!(&mut buffer, "{} ", self.token_literal()).unwrap();
+        write!(&mut buffer, "{} = ", self.name).unwrap();
+        //TODO: add the value part
+        //write!(&mut buffer, "{};", self.value);
+
+        write!(f, "{}", buffer)
+    }
+}
+
 pub struct Identifier {
     pub token: Token,
     pub value: String,
@@ -112,6 +160,12 @@ impl Node for Identifier {
 
     fn node_type(&self) -> NodeType {
         NodeType::Identifier
+    }
+}
+
+impl fmt::Display for Identifier {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.value)
     }
 }
 
@@ -130,3 +184,36 @@ impl Node for ReturnStatement {
         NodeType::Return
     }
 }
+
+impl fmt::Display for ReturnStatement {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { 
+        let mut buffer = String::new();
+        write!(&mut buffer, "{} ", self.token_literal()).unwrap();
+        //TODO: add the return_value part 
+        //write!(&mut buffer, "{};", self.name).unwrap();
+        write!(f, "{}", buffer)
+    }
+}
+
+// for this kind of statements -> x + 10;
+pub struct ExpressionStatement {
+    pub token : Token,
+    pub expression: Expression,
+}
+
+impl Node for ExpressionStatement {
+    fn token_literal(&self) -> &str {
+        &self.token.literal
+    }
+
+    fn node_type(&self) -> NodeType {
+        NodeType::ExpressionStatement
+    }
+}
+
+impl fmt::Display for ExpressionStatement {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.expression)
+    }
+}
+
